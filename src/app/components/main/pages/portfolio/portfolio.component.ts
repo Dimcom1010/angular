25d0810ca994fa2ff2eb14011eaeb5a6da259dfd
Dimcom1010 <dimcom1010@gmail.com>
2023-06-20 23:13:36 +0300

    add featch slider page

--- src/app/components/main/pages/portfolio/portfolio.component.ts
+++ src/app/components/main/pages/portfolio/portfolio.component.ts
@@ -2,25 +2,36 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { GetFotosService } from './services/get-fotos.service';
 import { HttpClientModule } from '@angular/common/http';
-
+import { SliderComponent } from './components/slider/slider.component';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.less'],
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule,SliderComponent],
 })
 export class PortfolioComponent implements OnInit {
+  isOpenSlider$=new BehaviorSubject<boolean>(false)
+  selectedPhotoID$=new BehaviorSubject<number>(1)
   isLoading:boolean=false;
+
   photos: any;
-  f$: any;
+
   constructor(private _fotoService: GetFotosService) {}
    ngOnInit(): void {
     this.photos = this._fotoService.getFotos();
 
   }
-  show(id:number){
-    console.log('test',id)
+  openSlider(id:number){
+    console.log('test',id);
+    this.isOpenSlider$.next(true);
+    this.selectedPhotoID$.next(id);
   }
+  closeSlider(){
+    this.isOpenSlider$.next(false)
+  }
+
+
 }
