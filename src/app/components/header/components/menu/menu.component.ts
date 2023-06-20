5a0996d70aee85921b51ac8f0e77fa2e2a660d74
d.uschenko <d.uschenko@gmail.com>
2023-06-20 15:34:20 +0300

    меню бургер

--- src/app/components/header/components/menu/menu.component.ts
+++ src/app/components/header/components/menu/menu.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../button/button.component';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule,ButtonComponent],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.less']
+  styleUrls: ['./menu.component.less'],
 })
 export class MenuComponent {
-
+  isActive$ = new BehaviorSubject<boolean>(false);
+  menuTrigger() {
+    this.isActive$.next(!this.isActive$.value);
+    console.log(this.isActive$.value);
+  }
 }
